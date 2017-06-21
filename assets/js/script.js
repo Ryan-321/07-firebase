c8540fdb7126b0c648bb9888b806fcd61aeb0fb2
Ryan Kahle <jrkahle@hotmail.com>
2017-06-21 12:21:27 -0400

    reworks logic, trains updated on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/* global moment firebase */
+
 var db = firebase.database()
 
 // event listeners
@@ -6,7 +8,6 @@ document.getElementById('submit-btn').addEventListener('click', handleClick)
 function handleClick (event) {
   event.preventDefault()
   var data = getData()
-  console.log(data)
   db.ref().push(data)
   document.getElementById('form').reset()
 }
@@ -23,43 +24,55 @@ db.ref().on('child_added', function (snapshot) {
   document.getElementById('table').appendChild(tr)
 })
 
+// update times in DB for current time
+db.ref().once('value').then(function (snapshot) {
+  for (var key in snapshot.val()) {
+    var arrival = snapshot.val()[key].d_next_arrival
+    var freq = snapshot.val()[key].c_frequency
+    var formattedArrival = moment(arrival, 'h:mm a').format('HH:mm')
+    var na = nextArrival(formattedArrival, freq)
+    var min = minutesAway(na)
+    var updates = {}
+    updates.d_next_arrival = na
+    updates.e_min_until = min
+    // update time
+    db.ref(key).update(updates)
+  }
+})
+
 // functions
 
 function getData () {
   var data = {}
-  var firstTrain = document.getElementById('first-train').value
+  var arrival = document.getElementById('first-train').value
   data.a_name = document.getElementById('name').value
   data.b_destination = document.getElementById('destination').value
   data.c_frequency = document.getElementById('frequency').value
-  data.d_next_arrival = nextArrival(firstTrain, data.c_frequency)
-  data.e_min_until = remainder(minutesGT(firstTrain), data.c_frequency)
+  data.d_next_arrival = nextArrival(arrival, data.c_frequency)
+  data.e_min_until = minutesAway(data.d_next_arrival)
   return data
 }
 
-function nextArrival (ft, freq) {
-  var ct = moment()
-  var to = moment(ft, 'HH:mm')
-  if (ct > to) {
-    var diff = minutesGT(ft)
-    var over = remainder(diff, freq)
-    return moment().add(over, 'm').format('h:mm a')
+function nextArrival (train, freq) {
+  var currentTime = moment()
+  var arrival = moment(train, 'HH:mm')
+  if (currentTime.format('HH:mm') > arrival.format('HH:mm')) {
+    // difference in minutes from current time to first arrival
+      // divide that by freq, add that reminder to current time for next train
+    var diff = currentTime.diff(arrival, 'minutes') % freq
+    return moment().add(diff, 'm').format('h:mm a')
   } else {
-    return moment(ft, 'HH:mm').format('h:mm a')
+    // if arrival time is > than current, return arrival time
+    return moment(arrival, 'HH:mm').format('h:mm a')
   }
 }
 
-function minutesGT (ft) {
-  var ct = moment()
-  var to = moment(ft, 'HH:mm')
-  if (ct > to) {
-    return ct.diff(to, 'minutes')
-  } else {
-    return to.diff(ct, 'minutes')
-  }
-}
-
-function remainder (diff, freq) {
-  return diff % freq
+function minutesAway (train) {
+  var currentTime = moment()
+  var arrival = moment(train, 'HH:mm')
+  // amount in min arrival is greater than currentTime
+  var diff = arrival.diff(currentTime, 'minutes')
+  return diff
 }
 
 // Bonus material
